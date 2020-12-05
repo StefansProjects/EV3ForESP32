176cb19662e0e71ba0b11bbaf30321c8a422566d
Stefan Richter-Huber <stefan.huber@ymail.com>
2020-12-05 23:16:46 +0100

    First motor control impl

---
+++ doc/sensors/sensor-connection.md
@@ -0,0 +1,38 @@
+
+# Notes on Sensor connection
+
+## Pinout
+
+### EV3 Sensor
+
+NC -> 2
+GND -> 3,1
+VCC5V -> 4
+TX -> 6
+RX -> 5
+
+### NXT Sensor
+
+NC (9V, 5V Pullup) -> 1
+GND -> 3,2
+VCC5V -> 4
+SDA -> 6
+SCL -> 5
+
+### EV3 Motor
+
+Tacho1 (5V) -> 5
+Tacho2 (5V) -> 6
+GND -> 3
+VCC5V -> 4
+Motor1 -> 1
+Motor2 -> 2
+
+### Kabel
+
+Weiß (M1) -> 1
+Schwarz (M2) -> 2
+Rot (GND) -> 3
+Grün (5V) -> 4
+Gelb (DATA1) -> 5
+Blau (DATA2) -> 6
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 #include "Lpf2HubEmulation.h"
 #include "LegoinoCommon.h"
+#include "PowerFunctions.h"
+#include <Wire.h>
+#include <ESP32Encoder.h>
 
+const int motor1Pin1 = 27;
+const int motor1Pin2 = 26;
+const int tacho1Pin1 = 18;
+const int tacho1Pin2 = 19;
+int dutyCycle = 200;
 // create a hub instance
 Lpf2HubEmulation myEmulatedHub("TrainHub", HubType::POWERED_UP_HUB);
+
+ESP32Encoder encoder;
+
+//Specify the links and initial tuning parameters
+double Kp = 10, Ki = 0.0, Kd = 1;
+int target = 90;
+double cumError = 0.0;
+double lastError = 0.0;
+unsigned long previousTime;
+
+int consoleCnt = 0;
 
 void writeValueCallback(byte port, byte subcommand, std::string value)
 {
@@ -30,11 +49,35 @@ void writeValueCallback(byte port, byte subcommand, std::string value)
 
 void setup()
 {
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+
+  digitalWrite(motor1Pin2, LOW);
+
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(motor1Pin1, 0);
+
+  encoder.attachHalfQuad(tacho1Pin1, tacho1Pin2);
+
   Serial.begin(115200);
   // define the callback function if a write message event on the characteristic occurs
   Serial.println("Initalize virtual hub");
   myEmulatedHub.setWritePortCallback(&writeValueCallback);
   myEmulatedHub.start();
+}
+
+void setMotor(boolean forward, uint8_t value)
+{
+  if (forward)
+  {
+    digitalWrite(motor1Pin2, LOW);
+    ledcWrite(0, (value));
+  }
+  else
+  {
+    digitalWrite(motor1Pin2, HIGH);
+    ledcWrite(0, (255 - value));
+  }
 }
 
 void loop()
@@ -51,5 +94,60 @@ void loop()
     delay(1000);
     myEmulatedHub.attachDevice((byte)PoweredUpHubPort::B, DeviceType::TRAIN_MOTOR);
     delay(1000);
+  }
+
+  /*
+  while (dutyCycle <= 255)
+  {
+    ledcWrite(0, dutyCycle);
+    Serial.print("Forward with duty cycle: ");
+    Serial.println(dutyCycle);
+    dutyCycle = dutyCycle + 5;
+    delay(500);
+    Serial.print("Encoder count: ");
+    Serial.println(String((int32_t)encoder.getCount()));
+  }
+  dutyCycle = 0;
+  */
+
+  int32_t input = (int32_t)encoder.getCount();
+  unsigned long currentTime = millis();
+  unsigned long elapsedTime = currentTime - previousTime;
+
+  double error = (target - input);
+  cumError += error * elapsedTime;
+  double rateError = (error - lastError) / elapsedTime;
+
+  int32_t output = Kp * error + Ki * cumError + Kd * rateError;
+  if (output > 255)
+  {
+    output = 255;
+  }
+  if (output < -255)
+  {
+    output = -255;
+  }
+
+  setMotor(output >= 0, abs(output));
+  consoleCnt++;
+  lastError = error;
+  previousTime = currentTime;
+  delay(5);
+
+  if (consoleCnt == 200)
+  {
+    Serial.print("Encoder count: ");
+    Serial.println(String(input));
+    Serial.print("Set point: ");
+    Serial.println(String(target));
+    Serial.print("Forward with duty cycle: ");
+    Serial.println(output);
+
+    Serial.print("Error: ");
+    Serial.println(error);
+
+    Serial.print("Rate error: ");
+    Serial.println(rateError);
+    consoleCnt = 0;
   }
 }

ab750031bfc51c4450bb6afa0352d7fbee53662a
Stefan Richter-Huber <stefan.huber@ymail.com>
2020-12-16 22:39:20 +0100

    Added coast / brake to regulated motor

--- src/RegulatedMotor.h
+++ src/RegulatedMotor.h
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <ESP32Encoder.h>
 
+/**
+ * Types of PID regulations available for a regulated motor.
+ */
 enum struct RegulationType : uint8_t
 {
     NONE,
@@ -8,16 +11,26 @@ enum struct RegulationType : uint8_t
     SPEED
 };
 
+enum struct MotorState
+{
+    COAST,
+    FORWARD,
+    REVERSE,
+    BRAKE
+};
+
 class RegulatedMotor
 {
 private:
+    // Pins for the motors
     int _motorPin1;
     int _motorPin2;
     int _tachoPin1;
     int _tachoPin2;
 
+    // We use 8 bit pwm, defines maximum values
     const double PWM_MAX = 255;
-    const double PWM_MIN = -255;
+    const double PWM_MIN = -1 * PWM_MAX;
 
     /**
      * PID control variables for position control.
@@ -26,44 +39,55 @@ private:
     const double KI_POS_CTRL = -2.0;
     const double KD_POS_CTRL = 20.0;
 
-    /**
-     * PID control variables for position control.
-     */
+    // PID control parameters
     double _Kp = KP_POS_CTRL, _Ki = KI_POS_CTRL, _Kd = KD_POS_CTRL;
+    // PID error sum
     double _outputSum = 0.0;
 
+    // Quadrature encoder used to determine position
     ESP32Encoder encoder;
 
+    // Prevent simultanious access quadrature encoder
     SemaphoreHandle_t _positionMutex;
+    // Prevent simultanious access to speed variable
     SemaphoreHandle_t _speedMutex;
+    // Prevent simultanious access to pid parameters
     SemaphoreHandle_t _pidMutex;
+    // Handle for the motor control task.
     TaskHandle_t _motorCtrlHandle = NULL;
 
+    // Type of PID regulration currently active
     RegulationType _reg_type;
+    // Target value for the PID regulration
     int64_t _reg_target;
 
-    /**
-   * Previous position after before the last call
-   */
+    // Previous position after before the last call
     int64_t _prev_pos = 0;
-    /**
-   * Time of the last motor control loop
-   */
-    unsigned long _prev_time = 0;
-    /**
-   * Current speed of the motor
-   */
-    double _speed = 0;
 
-    /**
-   * @see https://www.freertos.org/FreeRTOS_Support_Forum_Archive/July_2010/freertos_Is_it_possible_create_freertos_task_in_c_3778071.html
-   * @see https://forum.arduino.cc/index.php?topic=674975.0
-   */
+    //Time of the last motor control loop
+    unsigned long _prev_time = 0;
+
+    //Current speed of the motor
+    double _speed = 0;
+    double _prev_speed = 0;
+    MotorState _motorState = MotorState::COAST;
+
+    /*
+     * Utility method to bind a class method to a FreeRTOS task.
+     * 
+     * @see https://www.freertos.org/FreeRTOS_Support_Forum_Archive/July_2010/freertos_Is_it_possible_create_freertos_task_in_c_3778071.html
+     * @see https://forum.arduino.cc/index.php?topic=674975.0
+     * 
+     * @param parm Reference to the actual RegulatedMotor object to handle.
+     */
     static void motorCtrlHelper(void *parm)
     {
         static_cast<RegulatedMotor *>(parm)->motorCtrl();
     }
 
+    /**
+     * Actual FreeRTOS task to control the motor. Determines current speed and performs PID operation.
+     */
     void motorCtrl()
     {
         for (;;)
@@ -118,6 +142,37 @@ private:
 
                 setPWM(output > 0, abs(output));
             }
+            else if (_reg_type == RegulationType::SPEED)
+            {
+                double output = 0;
+                double dInput = speed - _prev_speed;
+                double error = (double(_reg_target)) - ((double)speed);
+
+                _outputSum += (_Ki * error);
+                if (_outputSum > PWM_MAX)
+                {
+                    _outputSum = PWM_MAX;
+                }
+                else if (_outputSum < PWM_MIN)
+                {
+                    _outputSum = PWM_MIN;
+                }
+
+                output += _Kp * error;
+
+                output += _outputSum - _Kd * dInput;
+
+                if (output > PWM_MAX)
+                {
+                    output = PWM_MAX;
+                }
+                else if (output < PWM_MIN)
+                {
+                    output = PWM_MIN;
+                }
+
+                // TODO calculate pwm from output
+            }
             xSemaphoreGive(_pidMutex);
 
             _prev_pos = pos;
@@ -147,10 +202,12 @@ public:
         pinMode(_motorPin1, OUTPUT);
         pinMode(_motorPin2, OUTPUT);
 
-        digitalWrite(_motorPin1, LOW);
-
+        // Setup channel 0 with 5khz pwm freq with 8 bit resolution.
         ledcSetup(0, 5000, 8);
-        ledcAttachPin(_motorPin1, 0);
+
+        // Set motors to coast mode
+        digitalWrite(_motorPin1, LOW);
+        digitalWrite(_motorPin2, LOW);
 
         // Setup motor encoder
         encoder.attachFullQuad(_tachoPin1, _tachoPin2);
@@ -204,6 +261,9 @@ public:
         return speed;
     }
 
+    /**
+     * Manually configures a PID target type and the corresponding PID parameters.
+     */
     void set(RegulationType type, int64_t value, double Kp, double Ki, double Kd)
     {
         xSemaphoreTake(_pidMutex, portMAX_DELAY);
@@ -217,7 +277,7 @@ public:
     }
 
     /**
-    * Set raw pwm value
+    * Set raw pwm value for the motor.
     * @param direction forward = true, backward = false
     * @param value raw pwm value to set.
     */
@@ -225,18 +285,99 @@ public:
     {
         if (direction)
         {
-            digitalWrite(_motorPin2, LOW);
-            ledcWrite(0, (value));
+            if (_motorState == MotorState::REVERSE)
+            {
+                ledcDetachPin(_motorPin2);
+            }
+            if (_motorState != MotorState::FORWARD)
+            {
+                ledcAttachPin(_motorPin1, 0);
+                digitalWrite(_motorPin2, LOW);
+                _motorState = MotorState::FORWARD;
+            }
         }
         else
         {
-            digitalWrite(_motorPin2, HIGH);
-            ledcWrite(0, (255 - value));
+            if (_motorState == MotorState::FORWARD)
+            {
+                ledcDetachPin(_motorPin1);
+            }
+            if (_motorState != MotorState::REVERSE)
+            {
+                ledcAttachPin(_motorPin2, 0);
+                digitalWrite(_motorPin1, LOW);
+                _motorState = MotorState::REVERSE;
+            }
         }
+        ledcWrite(0, value);
     }
 
+    /**
+     * Set motor to coast mode (both h-bridge outputs high-Z)
+     */
+    void coast()
+    {
+        switch (_motorState)
+        {
+        case MotorState::COAST:
+            break;              // Already coasting
+        case MotorState::BRAKE: // Move from coasting to brake
+            digitalWrite(_motorPin1, LOW);
+            digitalWrite(_motorPin2, LOW);
+            break;
+        case MotorState::FORWARD:
+            ledcDetachPin(_motorPin1);
+            digitalWrite(_motorPin1, LOW);
+            digitalWrite(_motorPin2, LOW);
+            break;
+        case MotorState::REVERSE:
+            ledcDetachPin(_motorPin2);
+            digitalWrite(_motorPin1, LOW);
+            digitalWrite(_motorPin2, LOW);
+            break;
+        }
+        _motorState = MotorState::COAST;
+    }
+
+    /**
+     * Set motor to brake mode (both h-bridge outputs low)
+     */
+    void brake()
+    {
+        switch (_motorState)
+        {
+        case MotorState::BRAKE:
+            break;              // Already breaking
+        case MotorState::COAST: // Move from coasting to brake
+            digitalWrite(_motorPin1, HIGH);
+            digitalWrite(_motorPin2, HIGH);
+            break;
+        case MotorState::FORWARD:
+            ledcDetachPin(_motorPin1);
+            digitalWrite(_motorPin1, HIGH);
+            digitalWrite(_motorPin2, HIGH);
+            break;
+        case MotorState::REVERSE:
+            ledcDetachPin(_motorPin2);
+            digitalWrite(_motorPin1, HIGH);
+            digitalWrite(_motorPin2, HIGH);
+            break;
+        }
+        _motorState = MotorState::BRAKE;
+    }
+
+    /**
+     * Moves the motor to the given relative position (PID regulated.)
+     */
     void setPosition(int64_t position)
     {
         set(RegulationType::POSITION, position, KP_POS_CTRL, KI_POS_CTRL, KD_POS_CTRL);
+    }
+
+    /**
+     * Sets the speed of the motor (PID regulated)
+     */
+    void setSpeed(int32_t speed)
+    {
     }
 };

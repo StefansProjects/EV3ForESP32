f313923f76d40d9db0c38230d9af4ad401ffeeb5
Stefan Richter-Huber <stefan.huber@ymail.com>
2020-12-25 20:14:51 +0100

    First work on us sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include <Arduino.h>
-#include "EV3SensorPort.h"
-#include "EV3ColorSensor.h"
+#include "NXTUltrasonicSensor.h"
 
-const int motor1Pin1 = 27;
+const int motor1Pin1 = 27; // 9V Power for US sensor
 const int motor1Pin2 = 26;
-const int tacho1Pin1 = 18;
-const int tacho1Pin2 = 19;
+const int tacho1Pin1 = 18; // = SCL
+const int tacho1Pin2 = 19; // = SDA
+const int sclPin = 5;      // Additional SCL pin for sensor
 
-EV3SensorPort sensor(&Serial1, [](int v) { Serial1.begin(v, SERIAL_8N1, tacho1Pin2, tacho1Pin1); });
-EV3ColorSensor color(&sensor);
-EV3ColorSensorColor prev = EV3ColorSensorColor::NONE;
+NXTUltrasonicSensor s1 = NXTUltrasonicSensor(tacho1Pin2, tacho1Pin1, sclPin);
 
 void setup()
 {
@@ -18,34 +16,42 @@ void setup()
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
 
-  // Set motor outputs to coast mode
-  digitalWrite(motor1Pin1, LOW);
+  // Activate powersupply for ultrasonic sensor
+  digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
 
   // Serial comm to the host machine
   Serial.begin(115200);
 
-  /**
-   * This example expects the lego mindestorms color sensors.
-   */
-  sensor.begin([](EV3SensorPort *p) {
-    Serial.print("Found sensor of type ");
-    Serial.println(p->getCurrentConfig()->type, HEX);
-    vTaskDelay(50 / portTICK_PERIOD_MS);
-    color.setMode(EV3ColorSensorMode::COL_COLOR);
-    color.setOnColColor([](EV3ColorSensorColor c) {
-      if (c != prev)
-      {
-        prev = c;
-        Serial.print("Color ");
-        writeEV3ColorSensorColorToStream(c, &Serial);
-        Serial.println();
-      }
-    });
-  });
+  // Start the sensor
+  //s1.begin();
+  //s1.readVersion();
 }
 
 void loop()
 {
-  delay(50);
+  Wire.begin(tacho1Pin2, tacho1Pin1, 9768);
+  Wire.beginTransmission(0x02 >> 1);
+  Wire.write(0x0);
+  Wire.endTransmission(true);
+  Wire.flush();
+
+  delayMicroseconds(60); //Needed for receiving to work
+  pinMode(sclPin, OUTPUT);
+  digitalWrite(sclPin, LOW);
+  delayMicroseconds(34);
+  pinMode(sclPin, INPUT);
+  digitalWrite(sclPin, HIGH);
+  delayMicroseconds(60);
+
+  const auto l = Wire.requestFrom(0x02 >> 1, 8);
+  ESP_LOGV(TAG, "Read %d bytes from slave", l);
+
+  byte *result = new byte[8];
+  std::fill(result, result + 8, 0);
+  Wire.readBytes(result, l);
+
+  ESP_LOGV(TAG, "NXT ultrasonic sensor version: '%s'", result);
+
+  delay(250);
 }
